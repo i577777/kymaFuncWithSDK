0fb75fa9af822dcb53338f4be33eb7c2555fbd49
i577777 <148464019+i577777@users.noreply.github.com>
2023-11-17 17:38:21 +0100

    Update handler.js

--- i577777-custom-registry/src/handler.js
+++ i577777-custom-registry/src/handler.js
@@ -1,26 +1,4 @@
-const odataSdk = require("@sap-opps/odata-sdk");
-const util = require("util");
-
-const messages = [];
-
 module.exports = {
   main: async function (event, context) {
-    try {
-      const od = odataSdk.oData;
-      const odfn = od();
-      messages.push("Lets go! - GitHub version");
-      const resultPromise = await odfn.basePricesApi
-        .requestBuilder()
-        .getAll()
-        .top(5)
-        .execute({ destinationName: "PAULAN_ODATA_DEST" });
-      messages.push("Still alive!");
-      messages.push(resultPromise);
-      messages.push("done");
-    } catch (error) {
-      messages.push(error.message);
-    } finally {
-      return util.inspect(messages);
-    }
-  },
+    return "Hello from GitHub"
 };
